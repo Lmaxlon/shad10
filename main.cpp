8d4b9a7c46feca34c0bc67ff186bec75bfd2f1ba
Lmaxlon <max.vuckolov@yandex.ru>
2022-05-01 23:20:44 +0300

    upgrade

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 
 int main()
 {
-     int num;//количество вагонов
+     int coach;//количество вагонов
+     int num; //количество строк
      int column = 30;//количество столбцов
-     std::cin >> num;
-     num = 3 * num; //количество строк
-     int buffer = 0; //буфер который запоминает столбец
+     std::cin >> coach;
+     num = 3 * coach;
+     int free_places = 0; //счетчик количества свободных мест
      int counter = 0;//счетчик занятых мест
-     bool flag = false; //в случае если мест занято больше чем 1 - true
      char arr[num][column];
      for ( int i = 0; i < num; i++){
          for (int j = 0; j < column; j++){
@@ -23,27 +23,36 @@ int main()
               }
               std::cout << std::endl;//типа вывод для проверки
           }*/
-     for (int i = 0; i < num; i++){
-                   for (int j = 0; j < column; j++){
-                       buffer = j;//запомнили , после чего проверяем диапазон
-                       if(arr[i][j] == *"."){
-                           while(arr[i][j] != *"." || j < 30){
-
-                               if(arr[i][j] == *"X"){
-                                   counter++;//место занято
-                                   j++; //чекер следующего места (только в диапазоне)
-                               } else {//если место не занято
-                                   j++;//проверяем следующее место
-                               }
-                           if (counter > 1){
-                               flag = true;
-                               j = buffer;
-                               break;
-                           }
-
-                           }
-                       }
-                   }
-                   std::cout << std::endl;
-               }
+     for ( int i = 0; i < num; i++){
+              for (int j = 0; j < column; j++){
+                  if (arr[i][j] != *"." ){
+                      for (int m1 = 3; m1 < 9; m1++){
+                          if (arr[i][m1] != *"X" ){
+                              free_places++;
+                          } else counter++;
+                      }
+                      for (int m2 = 11; m2 < 17; m2++){
+                          if (arr[i][m2] != *"X" ){
+                              free_places++;
+                          } else counter++;
+                      }
+                      for (int m3 = 19; m3 < 25; m3++){
+                          if (arr[i][m3] != *"X" ){
+                              free_places++;
+                          } else counter++;
+                      }
+                      for (int m4 = 27; m4 < 30; m4++){
+                          if (arr[i][m4] != *"X" ){
+                              free_places++;
+                          } else counter++;
+                      }
+                      }
+                  }
+              }
+     if (counter > 1){
+         std::cout << "-1" << std::endl;
+     }
+     if (counter < 2){
+         std::cout << free_places << std::endl;
+     }
 }
