49bb46f31500bfe30fe0e40b39202e3d7658f8ce
Lmaxlon <max.vuckolov@yandex.ru>
2022-05-02 03:04:29 +0300

    version 2.0 - working with 1 coach

--- main.cpp
+++ main.cpp
@@ -5,19 +5,17 @@ int main()
 {
      int coach;//количество вагонов
      int num = 3; //количество строк
-     int column = 30;//количество столбцов
      std::cin >> coach;
      int free_places = 0; //счетчик количества свободных мест
-     int counter0 = 0;
      int counter1 = 0;//счетчик занятых мест
      int counter2 = 0;
      int counter3 = 0;
      int counter4 = 0;
-     char arr[num][column];
+     char arr[num][30];
      int huyna = num * coach;
      bool flag;
      for ( int i = 0; i < huyna; i++){
-         for (int j = 0; j < column; j++){
+         for (int j = 0; j < 30; j++){
              std::cin >> arr[i][j];
          }
      }
@@ -29,63 +27,55 @@ int main()
           }*/
           //3-9 11-17 19-25 27-30
      for ( int i = 0; i < num; i++){
-              if ( arr[i][0] == *"X") counter0++;
-              if (counter0 > 1){
-                  flag = true;
-                  std::cout << counter0 << std::endl;
-                  counter0 = 0;
-              }
               for (int j = 3; j < 9; j++){
+                       if (counter1 > 1){
+                           flag = true;
+                           counter1 = 0;
+                       }
                        if (arr[i][j] == *"X" ){
                            counter1++;
                        }
-                       if (counter1 > 1){
-                           flag = true;
-                           std::cout << counter1 << std::endl;
-                           counter1 = 0;
-                       }
                   }
               for (int j = 11; j < 17; j++){
+                  if (counter2 > 1){
+                     flag = true;
+                     counter2 = 0;
+                  }
                        if (arr[i][j] == *"X" ){
                            counter2++;
                        }
-                       if (counter2 > 1){
-                           flag = true;
-                           std::cout << counter2 << std::endl;
-                           counter2 = 0;
-                       }
                    }
               for (int j = 19; j < 25; j++){
+                  if (counter3 > 1){
+                      flag = true;
+                      counter3 = 0;
+              }
                        if (arr[i][j] == *"X" ){
                            counter3++;
                        }
-                       if (counter3 > 1){
-                           flag = true;
-                           std::cout << counter3 << std::endl;
-                           counter3 = 0;
-                       }
                    }
               for (int j = 27; j < 30; j++){
+                  if (counter4 > 1){
+                      flag = true;
+                      counter4 = 0;
+                  }
                        if (arr[i][j] == *"X" ){
                            counter4++;
                        }
-                       if (counter4 > 1){
-                           flag = true;
-                           std::cout << counter4 << std::endl;
-                           counter4 = 0;
-                       }
                    }
-              counter0 = 0;
               counter1 = 0;//счетчик занятых мест
               counter2 = 0;
               counter3 = 0;
               counter4 = 0;
               }
-         free_places = 44 - counter0 - counter1 - counter2 - counter3 - counter4;
+         for (int i = 0; i < num; i++){
+             for (int j = 0; j < 30; j++){
+                 if ( arr[i][j] == *"#") free_places++;
+             }
+         }
          if (flag == true) std::cout << "-1" << std::endl;
          if (flag == false) std::cout << free_places << std::endl;
          free_places = 0;
-              counter0 = 0;
               counter1 = 0;//счетчик занятых мест
               counter2 = 0;
               counter3 = 0;
@@ -96,4 +86,4 @@ int main()
 //#..X#X###..######..X###X#..#X#
 //#..#X####..#X####..#X####..##X
 //..............................
-//#..######..######..######..###
+//#..#X####..###X##..######..###
